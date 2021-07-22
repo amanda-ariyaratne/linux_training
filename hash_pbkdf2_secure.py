7588cb425fc570143a60e83aae81ebf81835981a
Amanda Ariyaratne <28347418+amanda-ariyaratne@users.noreply.github.com>
2021-07-22 16:45:38 +0530

    Update hash_pbkdf2_secure.py

--- hash_pbkdf2_secure.py
+++ hash_pbkdf2_secure.py
@@ -4,5 +4,6 @@ import hashlib
 
 password = str(sys.argv[1])
 byte_password = bytes(password, 'utf-8')
-dk = hashlib.pbkdf2_hmac('sha512', byte_password, os.urandom(16), 200000)
+salt = os.urandom(16)
+dk = hashlib.pbkdf2_hmac('sha512', byte_password, salt, 200000)
 print(dk.hex())
